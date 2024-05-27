ae03e106775836416596c9f2cf7fdd39cea75ec9
Andrei-DanieL86 <popandreidaniel1986@gmail.com>
2024-05-27 20:30:02 +0300

    model activation, db migration, user admin access + explore & and views creation

--- marketplace/marketplaceapp/models.py
+++ marketplace/marketplaceapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+#coloane: name, description, ingredients,price, weight_grams,
+# available_quantity, image
+class Product(models.Model):
+	name = models.CharField(max_length=255)
+	description = models.TextField()
+	ingredients = models.TextField()
+	price = models.DecimalField(max_digits=10, decimal_places=2) # adica sa imi afiseze cu exact 2 zecimale
+	weight_grams = models.IntegerField()
+	available_quantity = models.IntegerField(default=1)
+	image = models.ImageField(upload_to="product_images", null=True, blank=True) # locatia (folder) unde se vor salva imaginile uploadate din interfata
+
+	def __str__(self):
+		return self.name
+
+
+
+"""
+image = models.ImageField(upload_to="product_images", null=True, blank=True) 
+# locatia (folder) unde se vor salva imaginile uploadate din interfata
+# Poate sa nu fie nici una none=True
+#Poate sa fie si fara poza adica blank=True
+"""
+
--- marketplace/marketplaceapp/views.py
+++ marketplace/marketplaceapp/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
-
+from .models import Product
+from django.shortcuts import get_object_or_404
 
 def index(request):
 	return HttpResponse("Hello world")
+
+def product_list(request):
+	# read all products from db
+	all_products = Product.objects.all()
+	print(all_products)
+	return HttpResponse("Product list")
+
+def product_detail(request, product_id):
+	# read only one product
+	# product = Product.objects.get(pk=product_id) # pk vine de la primarykey
+	product = get_object_or_404(Product, pk=product_id)
+	print(product)
+	return HttpResponse(f"Product_detail {product_id}")
