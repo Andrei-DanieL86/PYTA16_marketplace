b76090b9aae083d79eb9a186c32b47e68075895f
Andrei-DanieL86 <popandreidaniel1986@gmail.com>
2024-05-29 20:22:34 +0300

    added html & css plus register app

--- marketplace/marketplace/urls.py
+++ marketplace/marketplace/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("marketplaceapp.urls"))
+    path("", include("marketplaceapp.urls")),
+    path("", include("register.urls"))
 ]
--- marketplace/marketplaceapp/views.py
+++ marketplace/marketplaceapp/views.py
@@ -1,19 +1,28 @@
 from django.http import HttpResponse
 from .models import Product
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 
 def index(request):
-	return HttpResponse("Hello world")
+	#return HttpResponse("Hello world")
+	return render(request, "marketplaceapp/home.html")
 
 def product_list(request):
 	# read all products from db
 	all_products = Product.objects.all()
+	context = {
+		"products" : all_products
+	}
 	print(all_products)
-	return HttpResponse("Product list")
+	#return HttpResponse("Product list")
+	return render(request, "marketplaceapp/product_list.html", context)
 
 def product_detail(request, product_id):
 	# read only one product
 	# product = Product.objects.get(pk=product_id) # pk vine de la primarykey
 	product = get_object_or_404(Product, pk=product_id)
 	print(product)
-	return HttpResponse(f"Product_detail {product_id}")
+	context = {
+		"product" : product
+	}
+	#return HttpResponse(f"Product_detail {product_id}")
+	return render(request, "marketplaceapp/product_detail.html", context)
